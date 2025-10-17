d870d940d6b7f643c29a390953adde5e662ce885
Thomas <trosas@icstars.org>
2025-10-17 18:34:58 -0500

    adding dev logs

---
+++ script.js
@@ -0,0 +1,27 @@
+let username = "Thomas"
+let number = "6";
+console.log(number );
+let numbers = 6;
+let x = number + numbers;
+console.log(x)
+let one = 1;
+let z = numbers + one;
+console.log(z);
+
+    let a = 22;
+    let b =10;
+function add(a,b){
+    
+    let c = a +b;
+    console.log(c);
+}
+add(33,22);
+console.log(x ===5);
+let word = false;
+let anotherWord = false;
+console.log(word ===anotherWord);
+function add(a,b,c){
+let sum = a + b +c;
+console.log(sum);
+}
+add(1,2,3);
